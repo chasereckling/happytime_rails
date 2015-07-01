0767aa6e32bedfbd89417bd0be0eaebc188a4cf1
chase reckling & kha le <student@epicodus.com>
2015-07-01 11:20:37 -0700

    add CRUD functionality to bars_controller  for 'bars', add abillity to add bars to index.

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -7,4 +7,37 @@ class BarsController < ApplicationController
   def new
     @bar = Bar.new
   end
+
+  def create
+    @bar = Bar.new(bar_params)
+    if @bar.save()
+      redirect_to bars_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bar = Bar.find(params[:id])
+    @bar.destroy
+    redirect_to bars_path
+  end
+
+  def edit
+    @bar = Bar.find(params[:id])
+  end
+
+  def update
+    @bar = Bar.find(params[:id])
+    if @bar.update(bar_params)
+      redirect_to bars_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def bar_params
+    params.require(:bar).permit(:name, :happytime, :address, :ontap)
+  end
 end
